0105e1e98ab05ac1a4b109ed06bfdd837b9ab094
jason <jason.cheng@lileesystems.com>
2020-02-05 16:18:32 +0800

    add download html element method

--- src/app/pages/draw-roadmap/draw-roadmap.component.ts
+++ src/app/pages/draw-roadmap/draw-roadmap.component.ts
@@ -7,9 +7,22 @@ import domtoimage from 'dom-to-image';
 })
 export class DrawRoadmapComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
   }
-
+  downloadImage(){
+    const node = document.getElementById('draw-area');
+    domtoimage.toBlob(node)
+    .then((blob)=> {
+      const a = document.createElement('a');
+      document.body.appendChild(a);
+      const url = window.URL.createObjectURL(blob);
+      a.href = url;
+      a.download = 'roadmap';
+      a.click();
+      setTimeout(() => {
+        window.URL.revokeObjectURL(url);
+        document.body.removeChild(a);
+      }, 0)
+    });
+  }
 }
