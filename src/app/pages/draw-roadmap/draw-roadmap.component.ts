e49404c163e232a54d9b696aa5e9ba7f21951a3e
jason zheng <101104044@nccu.edu.tw>
2020-02-06 00:03:37 +0800

    add timeline style

--- src/app/pages/draw-roadmap/draw-roadmap.component.ts
+++ src/app/pages/draw-roadmap/draw-roadmap.component.ts
@@ -9,20 +9,20 @@ export class DrawRoadmapComponent implements OnInit {
 
   ngOnInit() {
   }
-  downloadImage(){
+  downloadImage() {
     const node = document.getElementById('draw-area');
     domtoimage.toBlob(node)
-    .then((blob)=> {
-      const a = document.createElement('a');
-      document.body.appendChild(a);
-      const url = window.URL.createObjectURL(blob);
-      a.href = url;
-      a.download = 'roadmap';
-      a.click();
-      setTimeout(() => {
-        window.URL.revokeObjectURL(url);
-        document.body.removeChild(a);
-      }, 0)
-    });
+      .then((blob) => {
+        const a = document.createElement('a');
+        document.body.appendChild(a);
+        const url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = 'roadmap';
+        a.click();
+        setTimeout(() => {
+          window.URL.revokeObjectURL(url);
+          document.body.removeChild(a);
+        }, 0)
+      });
   }
 }
