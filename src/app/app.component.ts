bb510fee0cfef96fd8fc6f598c43fdc4f563db55
jason <jason.cheng@lileesystems.com>
2020-02-06 10:45:16 +0800

    add google map setting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'roadmap-drawer';
+  lat: number = 25.0470782;
+  lng: number = 121.5139071;
+  zoomValue: number = 15;
+  ngOnInit(){
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ShareModule } from './share/share.module';
 import { DrawRoadmapComponent } from './pages/draw-roadmap/draw-roadmap.component';
 import { AboutComponent } from './pages/about/about.component';
+import { AgmCoreModule } from '@agm/core';
 @NgModule({
   declarations: [AppComponent,DrawRoadmapComponent,AboutComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ShareModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyA8F4_cdzaJDI7by8YBhOh85DfpAKFY_9U'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
